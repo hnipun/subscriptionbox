5ad20a8f0d4627c1e5de62514631c3e01cfa3085
hnipunCodify <nipun@codify.ai>
2019-05-26 09:11:12 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    IonApp,
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+} from '@ionic/react';
+import React, {Component} from 'react';
+import BoxDisplay from "./pages/BranchList"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        chomps: 0,
+    };
+    unchomp = () => {
+        if (this.state.chomps > 0) {
+            this.setState({
+                chomps: this.state.chomps -= 1,
+            });
+        }
+    };
+    chomp = () => {
+        this.setState({
+            chomps: this.state.chomps += 1,
+        });
+    };
+
+    render() {
+        return (
+            <IonApp>
+                <IonHeader>
+                    <IonToolbar color="primary">
+                        <IonTitle>Subscription Boxes</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent>
+                    <BoxDisplay></BoxDisplay>
+                </IonContent>
+            </IonApp>
+        );
+    }
 }
 
 export default App;
